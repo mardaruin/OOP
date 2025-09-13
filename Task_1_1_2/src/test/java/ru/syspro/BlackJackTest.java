e766698b4043ce5fe3fdd73eeb33e6ec1c204ea4
Marina Rudometova <m.rudometova@g.nsu.ru>
2025-09-13 22:32:06 +0700

    First ver game with tests

---
+++ Task_1_1_2/src/test/java/ru/syspro/BlackJackTest.java
@@ -0,0 +1,13 @@
+package ru.syspro;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+class BlackJackGameTest {
+
+}
