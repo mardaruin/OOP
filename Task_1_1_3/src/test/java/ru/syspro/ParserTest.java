19a0f93e5c877ac3446475a60ce11756a0868ff3
Marina Rudometova <m.rudometova@g.nsu.ru>
2025-10-08 17:09:48 +0700

    parsertest fixed

--- Task_1_1_3/src/test/java/ru/syspro/ParserTest.java
+++ Task_1_1_3/src/test/java/ru/syspro/ParserTest.java
@@ -33,85 +33,78 @@ public class ParserTest {
     @Test
     public void testParseConstant() {
         Expression expr = new Parser("5").parse();
-        assertTrue(expr instanceof Number);
-        assertEquals(5, ((Number) expr).getValue());
+        var expected = new Number(5);
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testParseVariable() {
         Expression expr = new Parser("x").parse();
-        assertTrue(expr instanceof Variable);
-        assertEquals("x", ((Variable) expr).getName());
+        var expected = new Variable("x");
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testParseAddOperation() {
         Expression expr = new Parser("x+y").parse();
-        assertTrue(expr instanceof Add);
-        assertTrue(((BinaryOperation) expr).left instanceof Variable);
-        assertTrue(((BinaryOperation) expr).right instanceof Variable);
+        var expected = new Add(new Variable("x"), new Variable("y"));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testParseSubtractOperation() {
         Expression expr = new Parser("z-x").parse();
-        assertTrue(expr instanceof Sub);
-        assertTrue(((BinaryOperation) expr).left instanceof Variable);
-        assertTrue(((BinaryOperation) expr).right instanceof Variable);
+        var expected = new Sub(new Variable("z"), new Variable("x"));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testParseMultiplyOperation() {
         Expression expr = new Parser("x*y").parse();
-        assertTrue(expr instanceof Mul);
-        assertTrue(((BinaryOperation) expr).left instanceof Variable);
-        assertTrue(((BinaryOperation) expr).right instanceof Variable);
+        var expected = new Mul(new Variable("x"), new Variable("y"));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testParseDivideOperation() {
         Expression expr = new Parser("z/y").parse();
-        assertTrue(expr instanceof Div);
-        assertTrue(((BinaryOperation) expr).left instanceof Variable);
-        assertTrue(((BinaryOperation) expr).right instanceof Variable);
+        var expected = new Div(new Variable("z"), new Variable("y"));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testParseNestedOperations() {
         Expression expr = new Parser("x+(y*z)").parse();
-        assertTrue(expr instanceof Add);
-        assertTrue(((BinaryOperation) expr).left instanceof Variable);
-        assertTrue(((BinaryOperation) expr).right instanceof Mul);
+        var expected = new Add(new Variable("x"), new Mul(new Variable("y"), new Variable("z")));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testParseParenthesizedExpressions() {
         Expression expr = new Parser("(x+y)*z").parse();
-        assertTrue(expr instanceof Mul);
-        assertTrue(((BinaryOperation) expr).left instanceof Add);
-        assertTrue(((BinaryOperation) expr).right instanceof Variable);
+        var expected = new Mul(new Add(new Variable("x"), new Variable("y")), new Variable("z"));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testParseNegativeNumbers() {
         Expression expr = new Parser("0-x").parse();
-        assertTrue(expr instanceof Sub);
-        assertTrue(((BinaryOperation) expr).left instanceof Number);
-        assertTrue(((BinaryOperation) expr).right instanceof Variable);
+        var expected = new Sub(new Number(0), new Variable("x"));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testParseWhitespaceHandling() {
         Expression expr = new Parser("  x  ").parse();
-        assertTrue(expr instanceof Variable);
-        assertEquals(((Variable) expr).getName(), "x");
+        var expected = new Variable("x");
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testParseUnrecognizedVariables() {
         Expression expr = new Parser("w").parse();
-        assertTrue(expr instanceof Variable);
-        assertEquals(((Variable) expr).getName(), "w");
+        var expected = new Variable("w");
+        assertEquals(expected, expr);
     }
 
     @Test
@@ -122,50 +115,50 @@ public class ParserTest {
     @Test
     public void testSimpleAdditionWithoutBrackets() {
         Expression expr = new Parser("x+y+z").parse();
-        assertTrue(expr instanceof Add);
-        assertEquals(9, expr.eval(variables));
+        var expected = new Add(new Add(new Variable("x"), new Variable("y")), new Variable("z"));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testSimpleSubtractionWithoutBrackets() {
         Expression expr = new Parser("z-y-x").parse();
-        assertTrue(expr instanceof Sub);
-        assertEquals(-1, expr.eval(variables));
+        var expected = new Sub(new Sub(new Variable("z"), new Variable("y")), new Variable("x"));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testMixedOperationsWithoutBrackets() {
         Expression expr = new Parser("x+y-z").parse();
-        assertTrue(expr instanceof Sub);
-        assertEquals(1, expr.eval(variables));
+        var expected = new Sub(new Add(new Variable("x"), new Variable("y")), new Variable("z"));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testMultiplicationAndDivisionWithoutBrackets() {
         Expression expr = new Parser("z*y/x").parse();
-        assertTrue(expr instanceof Div);
-        assertEquals(6, expr.eval(variables));
+        var expected = new Div(new Mul(new Variable("z"), new Variable("y")), new Variable("x"));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testComplexArithmeticSequence() {
         Expression expr = new Parser("x+y*z/x").parse();
-        assertTrue(expr instanceof Add);
-        assertEquals(8, expr.eval(variables));
+        var expected = new Add(new Variable("x"), new Div(new Mul(new Variable("y"), new Variable("z")), new Variable("x")));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testMultipleOperatorsInRow() {
         Expression expr = new Parser("x+x*x").parse();
-        assertTrue(expr instanceof Add);
-        assertEquals(6, expr.eval(variables));
+        var expected = new Add(new Variable("x"), new Mul(new Variable("x"), new Variable("x")));
+        assertEquals(expected, expr);
     }
 
     @Test
     public void testNegationInChain() {
         Expression expr = new Parser("0-(x+y)").parse();
-        assertTrue(expr instanceof Sub);
-        assertEquals(-5, expr.eval(variables));
+        var expected = new Sub(new Number(0), new Add(new Variable("x"), new Variable("y")));
+        assertEquals(expected, expr);
     }
 
 }
